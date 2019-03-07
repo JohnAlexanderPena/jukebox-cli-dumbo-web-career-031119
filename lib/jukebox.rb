1e547d57333a88e446b7b48404a040a2f77df172
John Pena <jpena91@gmail.com>
2019-03-07 05:17:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,8 +29,8 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
-  if (1..9).to_a.include?(input.to_i)   1
-    puts "Playing #{songs[input.to_i - 1]}"
+  if (1..9).to_a.include?(input.to_i) 
+    puts "Playing #{songs[input.to_i ]}"
   elsif songs.include?(input)
     puts "Playing #{input}"
   else
